bbcb7b2012b4675d43e479dc152edba81ce79ec1
Jan Vorcak <vorcak@webscope.io>
2018-05-13 01:20:55 +0200

    --amend

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,6 @@
 // @flow
 
-const isEmptyAtPosition = (word: string, position: number) => {
-  return word[position] === ' '
-}
+const isEmptyAtPosition = (word: string, position: number) => word[position] === ' '
 
 export const getWordStartAndEndLocation = (word: string, position: number) => {
   if (isEmptyAtPosition(word, position) && isEmptyAtPosition(word, position - 1)) {
@@ -22,17 +20,17 @@ export const getWordStartAndEndLocation = (word: string, position: number) => {
 }
 
 const insertSymbolInWord = (word: string, position: number, [prefix, suffix]: Array<string>) => {
-  const [ start, end ] = getWordStartAndEndLocation(word, position)
+  const [start, end] = getWordStartAndEndLocation(word, position)
   const startWord = word.slice(0, start)
   const actualWord = word.slice(start, end)
   const endWord = word.slice(end, word.length)
 
   if (actualWord.startsWith(prefix) && actualWord.endsWith(suffix)) {
     const strippedWord = actualWord.slice(prefix.length, actualWord.length - suffix.length)
-    return [ `${startWord}${strippedWord}${endWord}`, false ]
+    return [`${startWord}${strippedWord}${endWord}`, false]
   }
 
-  return [ `${startWord}${prefix}${actualWord}${suffix}${endWord}`, true ]
+  return [`${startWord}${prefix}${actualWord}${suffix}${endWord}`, true]
 }
 
 // reduce helper
@@ -41,14 +39,14 @@ function sum(total, num) {
 }
 
 export const insertSymbol = (text: string, position: number, [prefix, suffix]: Array<string>) => {
-
   const lines = text.split('\n')
   const lineLengths = lines.map(line => line.length)
 
   const currentLine = text.substring(0, position).split('\n').length - 1
 
   const relativeLinePosition: number = position - lineLengths.slice(0, currentLine).reduce(sum, 0)
-  const [newLine, replaced] = insertSymbolInWord(lines[currentLine], relativeLinePosition, [prefix, suffix])
+  const [newLine, replaced] =
+    insertSymbolInWord(lines[currentLine], relativeLinePosition, [prefix, suffix])
 
   lines[currentLine] = newLine
   return [lines.join('\n'), replaced]
