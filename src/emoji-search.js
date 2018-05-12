9c51c8e44cc1d7cac0004e977ec85b36fa3224ab
Jan Vorcak <vorcak@webscope.io>
2018-05-13 01:54:32 +0200

    Remove babel-polyfill from the main project

--- src/emoji-search.js
+++ src/emoji-search.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/extensions
 import emojilib from 'emojilib/emojis'
 import sort from 'match-sorter'
-import 'babel-polyfill' // TMP solution
 
 /* build proper library with included name of the emoji */
 const library = Object.entries(emojilib).map(([name, emojiObject]) =>
